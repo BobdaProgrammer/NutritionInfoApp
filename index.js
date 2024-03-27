f3311151822789686d041fbb8fb44a7b3d85130f
BobdaProgrammer <144843651+BobdaProgrammer@users.noreply.github.com>
2024-03-27 08:46:07 +0000

    more features

--- index.js
+++ index.js
@@ -59,6 +59,20 @@ function custom(event) {
               }
 }
 
+function score(lett, scoreType) {
+          let nutri = "";
+          nutri += `<span id='nutri'>${scoreType}</span>`;
+          nutri +=
+            lett == "a"
+              ? "<label for='nutri' style='color:green;'>A</label>"
+              : lett == "b"
+              ? "<label for='nutri' style='color:blue;'>B</label>"
+              : lett == "c"
+              ? "<label for='nutri' style='color:orange;'>C</label>"
+          : `<label for='nutri' style='color:red;'>${lett.toUpperCase()}</label>`;
+  return nutri
+}
+
 function success(result) {
   let resultEl = document.getElementById("result");
   resultEl.style.display = "block";
@@ -83,8 +97,14 @@ function success(result) {
         let name = "";
         let val = 0;
         let serve = false;
-
-        resultEl.innerHTML = `<center><button onclick="location.reload()">Scan new item</button><h2 id="name">${data["product"]["product_name"]}</h2><h5>Last updated: ${data["product"]["last_edit_dates_tags"][0]}</h5>Add to:<br><button onclick="meal('breakfast')">Breakfast</button><button onclick="meal('lunch')">Lunch</button><button onclick="meal('dinner')">Dinner</button><button onclick="meal('snack')">Snack</button><br><input value=100 style="margin-top: 8px" class="selector" type="number" onchange="custom(event)" min=0>g</center><div id="custom"></div><h4>Typical values per 100g:</h4></center>`;
+        let allergens = data["product"]["allergens_from_ingredients"]
+        resultEl.innerHTML = `<center><button onclick="location.reload()">Scan new item</button><h2 id="name">${
+          data["product"]["product_name"]
+        }</h2><h5>Last updated: ${
+          data["product"]["last_edit_dates_tags"][0]
+        }</h5>Add to:<br><button onclick="meal('breakfast')">Breakfast</button><button onclick="meal('lunch')">Lunch</button><button onclick="meal('dinner')">Dinner</button><button onclick="meal('snack')">Snack</button><br><input value=100 style="margin-top: 8px" class="selector" type="number" onchange="custom(event)" min=0>g</center><div id="custom"></div></center><br>Additives: ${
+          data["product"]["additives_n"]
+        }<br>Allergens found in ingredients: ${allergens}<br><h4>Typical values per 100g:</h4>`;
         let res = `<div id="hundred">`
         // Loop through the keys of the nutriments object
         Object.keys(nutriments).forEach((key) => {
@@ -135,17 +155,9 @@ function success(result) {
             );
         }
         let nutriscore = data["product"]["nutriscore_grade"];
-        let nutri = "";
-        nutri += "<span id='nutri'>Nutri Score: </span>";
-        nutri +=
-          nutriscore == "a"
-            ? "<label for='nutri' style='color:green;'>A</label>"
-            : nutriscore == "b"
-            ? "<label for='nutri' style='color:blue;'>B</label>"
-            : nutriscore == "c"
-            ? "<label for='nutri' style='color:orange;'>C</label>"
-            : `<label for='nutri' style='color:red;'>${nutriscore.toUpperCase()}</label>`;
-        res += nutri;
+        res += score(nutriscore, "Nutri Score: ")+"<br>";
+        let ecoscrore = data["product"]["ecoscore_grade"]
+        res += score(ecoscrore, "Eco Score: ")
         resultEl.innerHTML+=res
       })
       .catch(error => {
